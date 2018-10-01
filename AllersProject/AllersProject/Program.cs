ed73b888c10692bebee00e3e0a0ade5297d249a3
danielgalvis98 <dangaltor@gmail.com>
2018-10-01 03:56:50 -0500

    fsdfs

--- AllersProject/AllersProject/Program.cs
+++ AllersProject/AllersProject/Program.cs
@@ -20,15 +20,23 @@ namespace AllersProject
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
             DataManager data = new DataManager("../../../Datos/");
-            AssociationAnalyzer aA = new AssociationAnalyzer(data,28,0.05,0.05,3);
+            AssociationAnalyzer aA = new AssociationAnalyzer(data,100, 0.05,0.05,3);
             Debug.WriteLine("ItsBeenASuccesThereAre " + data.getTransactionsCount() + "Transactions!");
             Debug.WriteLine(data.getCustomersCount() + "Customers!");
             Debug.WriteLine(data.getItemsCount() + "Items!");
             Debug.WriteLine(aA.getBinaryTransactions().Count + "binaryTransactions!");
-            aA.GenerateFrequentItemSets();
-            //Debug.WriteLine("EMPEZANDO------------------------");
-            //Debug.WriteLine(aA.GenerateFrequentItemSetsApriori(aA.CommonItems()).Count);
-            //Debug.WriteLine("TERMINANDO------------------------");
+            //aA.GenerateFrequentItemSets();
+            Debug.WriteLine("EMPEZANDO------------------------");
+            int starTime = DateTime.Now.Second;
+            List<List<long>>list = aA.GenerateFrequentItemSetsApriori();
+            Debug.WriteLine(list.Count);
+            foreach(List<long> lis in list)
+            {
+                Debug.WriteLine(lis.Count());
+            }
+            Debug.WriteLine(DateTime.Now.Second - starTime + "");
+
+            Debug.WriteLine("TERMINANDO------------------------");
         }
     }
 }
