9feb208bc29d528f0f76acff8b659836dbc55a13
BeadierDark <35853208+BeadierDark@users.noreply.github.com>
2018-10-07 22:31:38 -0500

    esqueleto kmeans

---
+++ AllersProject/Modelo/Cluster.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class Cluster
+    {
+        private int id;
+        private HashSet<double[]> cluster;
+        private double[] centroid;
+
+        public Cluster()
+        {
+
+        }
+
+
+        public void AddDataPoint(double[] dp)
+        {
+
+        }
+        public void RemoveAll ()
+        {
+
+        }
+        public void ComputeNewCentroid ()
+        {
+
+        }
+        public Boolean ContainsDataPoint (double[] dp)
+        {
+
+            return false;
+        }
+    }
+}
---
+++ AllersProject/Modelo/SimilarityAnalysisKMeans.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class SimilarityAnalysisKMeans
+    {
+        private int dimensionOfDataPoints;
+        private int numberOfClusters;
+        private List<double[]> dataPoints;
+        private int numberOfIterations;
+        private int minimumNumberOfItemsPerCustomer;
+        private Dictionary<double[], Customer> mapFromDataPointToCustomer;
+        private List<Cluster> clusters;
+
+        public SimilarityAnalysisKMeans ()
+        {
+
+        }
+
+        public double AngularDistance (double[] x, double[] y)
+        {
+            return 0.0;
+        }
+
+        public List<Tuple<Customer, double[], int>> GiveCustomers ()
+        {
+            return null;
+        }
+        public void Kmeans ()
+        {
+
+        }
+    }
+}
