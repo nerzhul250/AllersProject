ecd0cbf8413d6d8257169fc8c4089ccfb32a10bf
danielgalvis98 <dangaltor@gmail.com>
2018-10-13 18:31:27 -0500

    Pruebas separadas

--- AllersProject/UnitTestProject1/TestArbolFP.cs
+++ AllersProject/UnitTestProject1/TestArbolFP.cs
@@ -31,12 +31,64 @@ namespace UnitTestProject1
             prueba = new ArbolFP(transacc, 0.5);
         }
 
+
+
+
         [TestMethod]
         public void testNewFP()
         {
+            setEscenario1();
             Nodo raiz = prueba.Raiz;
+            Assert.AreEqual(raiz.hijos.Count, 2);
             Nodo prim = raiz.hijos["a"];
             Nodo sec = raiz.hijos["c"];
+
+            Assert.AreEqual(prim.Padre, raiz);
+            Assert.AreEqual(sec.Padre, raiz);
+            Assert.AreEqual(sec.Ocurrencia, 4);
+            Assert.AreEqual(prim.Ocurrencia, 1);
+
+            Assert.AreEqual(prim.hijos.Count, 1);
+            Nodo prim2 = prim.hijos["d"];
+            Assert.AreEqual(prim2.Ocurrencia, 1);
+            Assert.AreEqual(prim2.Padre, prim);
+
+            Assert.AreEqual(sec.hijos.Count, 2);
+            Nodo sec1 = sec.hijos["d"];
+            Nodo sec2 = sec.hijos["a"];
+            Assert.AreEqual(sec1.Ocurrencia, 1);
+            Assert.AreEqual(sec2.Ocurrencia, 2);
+            Assert.AreEqual(sec1.Padre, sec);
+            Assert.AreEqual(sec2.Padre, sec);
+
+            Assert.AreEqual(prim2.hijos.Count, 0);
+            Assert.AreEqual(sec1.hijos.Count, 0);
+
+            Assert.AreEqual(sec2.hijos.Count, 1);
+            Nodo sec21 = sec2.hijos["d"];
+            Assert.AreEqual(sec21.Ocurrencia, 1);
+            Assert.AreEqual(sec21.Padre, sec2);
+
+            Assert.AreEqual(sec21.hijos.Count, 0);
+
+            Dictionary<string, Nodo> listaEn = prueba.primeroListaEnlazada;
+            Nodo A1 = listaEn["a"];
+            Assert.AreEqual(A1, sec2);
+            Nodo A2 = A1.Siguiente;
+            Assert.AreEqual(A2, prim);
+            Assert.IsNull(A2.Siguiente);
+
+            Nodo C1 = listaEn["c"];
+            Assert.AreEqual(C1, sec);
+            Assert.IsNull(C1.Siguiente);
+
+            Nodo D1 = listaEn["d"];
+            Assert.AreEqual(D1, sec21);
+            Nodo D2 = D1.Siguiente;
+            Assert.AreEqual(D2, prim2);
+            Nodo D3 = D2.Siguiente;
+            Assert.AreEqual(D3, sec1);
+            Assert.IsNull(D3.Siguiente);
         }
     }
 }
